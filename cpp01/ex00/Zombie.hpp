c188b08d1219214626da743365ce9dc6d1dd6123
tmoel1 <troy_m@live.com.au>
2025-05-21 15:55:42 +0200

    add zombie class

--- cpp01/ex00/Zombie.hpp
+++ cpp01/ex00/Zombie.hpp
@@ -1,1 +1,23 @@
-// zombie class header
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <string>
+
+class Zombie
+{
+	public:
+		Zombie(); //need to add a string as param?? does it need to be explicit??
+		~Zombie();
+
+		void	announce() const; //const as this will simply print a msg - public or private??
+
+	private:
+		std::string _name; //better private?? 
+};
+
+
+
+
+
+
+#endif
