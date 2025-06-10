9b5e2ee302d7db5fb502c77818c19c309999780b
tmoel1 <140944173+tmoel1@users.noreply.github.com>
2025-06-10 16:33:28 +0200

    update cpp03 ex00 main, add cpp03 ex01 file structure

---
+++ cpp03/ex01/ClapTrap.hpp
@@ -0,0 +1,32 @@
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <string>
+
+class ClapTrap
+{
+	public:
+		ClapTrap();
+		ClapTrap(const std::string& name);
+		ClapTrap(const ClapTrap& other);
+		ClapTrap& operator=(const ClapTrap& rhs);
+		~ClapTrap();
+
+		void	attack(const std::string& target);
+		void	takeDamage(unsigned int amount);
+		void	beRepaired(unsigned int amount);
+
+	protected:
+		std::string		_name;
+		int				_hitPoints;
+		int				_energyPoints;
+		int				_attackDamage;
+
+		static const int	_kDefaultHealth	= 10;
+		static const int	_kDefaultEnergy	= 10;
+		static const int	_kDefaultAttack	= 0;
+
+	private:
+};
+
+#endif
