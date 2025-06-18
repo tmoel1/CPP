bbc88395c7cd6d04924ce719b37c25031761780a
tmoel1 <140944173+tmoel1@users.noreply.github.com>
2025-06-18 22:25:36 +0200

    update cpp05 ex00 class

---
+++ cpp05/ex00/Bureaucrat.cpp
@@ -0,0 +1,79 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() : _name("Anonymous Intern"), _grade(150) {}
+
+Bureaucrat::Bureaucrat(const std::string&name, int grade) : _name(name), _grade(grade)
+{
+	_validateGrade(grade);
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& other) : _name(other._name), _grade(other._grade) {}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& rhs)
+{
+	if (this != &rhs)
+		_grade = rhs._grade;
+
+	return *this;
+}
+
+Bureaucrat::~Bureaucrat() {}
+
+
+
+const std::string&	Bureaucrat::getName() const
+{
+	return _name;
+}
+
+int					Bureaucrat::getGrade() const
+{
+	return _grade;
+}
+
+void				Bureaucrat::incremenentGrade()
+{
+	_validateGrade(_grade - 1);
+
+	--_grade;
+}
+
+void				Bureaucrat::decrementGrade()
+{
+	_validateGrade(_grade + 1);
+
+	++_grade;
+}
+
+const char*		Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return "Bureaucrat: grade too high (must be >= 1)";
+}
+
+const char*		Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return "Bureaucrat: grade too low (must be <= 150)";
+}
+
+std::ostream&	operator<<(std::ostream& os, const Bureaucrat& b)
+{
+	return os << b.getName() << ", bureaucrat grade " << b.getGrade() << '.';
+}
+
+
+
+
+
+
+
+
+
+
+void	Bureaucrat::_validateGrade(int grade)
+{
+	if (grade < 1)
+		throw GradeTooHighException();
+	if (grade > 150)
+		throw GradeTooLowException();
+}
+
---
+++ cpp05/ex00/Bureaucrat.hpp
@@ -0,0 +1,42 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <string>
+#include <iostream>
+
+class Bureaucrat
+{
+	public:
+		Bureaucrat();
+		Bureaucrat(const std::string&name, int grade);
+		Bureaucrat(const Bureaucrat& other);
+		Bureaucrat& operator=(const Bureaucrat& rhs);
+		~Bureaucrat();
+
+		const std::string&	getName() const;
+		int					getGrade() const;
+		void				incremenentGrade();
+		void				decrementGrade();
+
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				const char* what() const throw(); //do these have to live on the same line?
+		};
+
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				const char* what() const throw();
+		};
+
+	private:
+		static void	_validateGrade(int Grade);
+
+		const std::string	_name;
+		int					_grade;
+};
+
+std::ostream&	operator<<(std::ostream& os, const Bureaucrat& b);
+
+#endif
