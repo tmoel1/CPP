094cb8fdf56060ab40c03c252f9fe31a90a81121
tmoel1 <140944173+tmoel1@users.noreply.github.com>
2025-07-29 19:59:25 +0200

    add cpp08 ex02

---
+++ cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,45 @@
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <stack>
+#include <list>
+#include <iostream>
+#include <string>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+	public:
+		MutantStack() {}
+		MutantStack(MutantStack const& other) : std::stack<T>(other) {}
+		MutantStack&	operator=(MutantStack const& rhs)
+		{
+			std::stack<T>::operator=(rhs);
+			return *this;
+		}
+		~MutantStack() {}
+
+		typedef typename std::stack<T>::container_type		base_container;
+
+		typedef typename base_container::iterator			iterator;
+		typedef typename base_container::const_iterator		const_iterator;
+
+		iterator	begin()
+		{
+			return this->c.begin();
+		}
+		iterator	end()
+		{
+			return this->c.end();
+		}
+		const_iterator	begin() const
+		{
+			return this->c.begin();
+		}
+		const_iterator	end() const
+		{
+			return this->c.end();
+		}
+};
+
+#endif
---
+++ cpp08/ex02/main.cpp
@@ -0,0 +1,105 @@
+#include "MutantStack.hpp"
+
+template <typename S>
+void printStack(S& s)
+{
+	typename S::iterator it  = s.begin();
+	typename S::iterator ite = s.end();
+	while (it != ite)
+	{
+		std::cout << *it << ' ';
+		++it;
+	}
+	std::cout << std::endl;
+}
+
+int main()
+{
+	std::cout << "Subject's Main Test (MutantStack):" << std::endl;
+	{
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(17);
+		std::cout << "Top: " << mstack.top() << std::endl;
+		mstack.pop();
+		std::cout << "Size: " << mstack.size() << std::endl;
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		mstack.push(0);
+
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+		
+		++it;
+		--it; // Testing both increment and decrement as required
+		
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::stack<int> s(mstack); // Proves it can be copied to a normal stack
+	}
+	std::cout << std::endl;
+
+	std::cout << "Comparison Test (std::list):" << std::endl;
+	{
+		std::list<int> mlist;
+		mlist.push_back(5);
+		mlist.push_back(17);
+		std::cout << "Top: " << mlist.back() << std::endl;
+		mlist.pop_back();
+		std::cout << "Size: " << mlist.size() << std::endl;
+		mlist.push_back(3);
+		mlist.push_back(5);
+		mlist.push_back(737);
+		mlist.push_back(0);
+
+		std::list<int>::iterator it = mlist.begin();
+		std::list<int>::iterator ite = mlist.end();
+
+		++it;
+		--it;
+
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::list<int> s(mlist);
+	}
+	std::cout << std::endl;
+
+	std::cout << "Copy and Assignment Test:" << std::endl;
+	{
+		MutantStack<int> original;
+		original.push(1);
+		original.push(2);
+		original.push(3);
+
+		MutantStack<int> copy(original);
+		MutantStack<int> assigned;
+		assigned = original;
+
+		original.pop();
+		
+		std::cout << "Original: "; printStack(original);
+		std::cout << "Copy:     "; printStack(copy);
+		std::cout << "Assigned: "; printStack(assigned);
+	}
+	std::cout << std::endl;
+
+	// --- String Test ---
+	// The helper function is perfect for this simple test
+	std::cout << "String Test:" << std::endl;
+	{
+		MutantStack<std::string> str_stack;
+		str_stack.push("Hello");
+		str_stack.push("World");
+		str_stack.push("!");
+		printStack(str_stack);
+	}
+
+	return 0;
+}
